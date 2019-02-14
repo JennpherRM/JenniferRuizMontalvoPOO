4298405f70f333e193c906f303b37e5b11d5975f
JennpherRM <46498091+JennpherRM@users.noreply.github.com>
2019-02-14 14:10:12 -0600

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class HolaMundo {
+
+	public static void main(String[]args){
+	String nombre;
+	Scanner scanner=new Scanner(System.in);
+	System.out.print("Escribe tu nombre ");
+	nombre=scanner.next();
+	System.out.println("Hola "+nombre);
+
+	}
+
+}
+
+
+
